99fab7bfb0a5f2484e77b4672aa09c5f0b121a79
yf1229 <yf1229@proton.me>
2024-11-25 07:31:47 +0900

    NodeDetailsScreen.kt, HomeScreen.kt にfloatingActionButton追加

--- app/src/main/java/com/example/pinode/compose/home/HomeScreen.kt
+++ app/src/main/java/com/example/pinode/compose/home/HomeScreen.kt
@@ -8,16 +8,25 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -26,11 +35,14 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pinode.PiNodeTopAppBar
 import com.example.pinode.R
 import com.example.pinode.data.Node
 import com.example.pinode.data.NodeStatus
@@ -53,14 +65,32 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
 ) {
     val homeUiState by viewModel.homeUiState.collectAsState()
-    // TODO scroll?
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
     Scaffold(
         modifier = modifier.padding(16.dp),
         topBar = {
-            // TODO
+            PiNodeTopAppBar(
+                title = stringResource(HomeDestination.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
         },
         floatingActionButton = {
-            // TODO
+            FloatingActionButton(
+                onClick = navigateToNodeEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier
+                    .padding(
+                        end = WindowInsets.safeDrawing.asPaddingValues()
+                            .calculateEndPadding(LocalLayoutDirection.current)
+                    )
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.node_entry_title)
+                )
+            }
         },
     ) { innerPadding ->
         HomeBody(
@@ -90,7 +120,6 @@ private fun HomeBody(
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun PiNodeList(
     itemList: List<Node>,
@@ -98,7 +127,6 @@ private fun PiNodeList(
     contentPadding: PaddingValues,
     modifier: Modifier = Modifier
 ) {
-    val haptics = LocalHapticFeedback.current
     LazyColumn(
         modifier = modifier,
         contentPadding = contentPadding
@@ -138,7 +166,7 @@ private fun DrawGrid(
     strokeWidth: Float,
     modifier: Modifier = Modifier,
 ) {
-    Canvas(modifier = Modifier
+    Canvas(modifier = modifier
         .background(Color.LightGray)
         .fillMaxSize()
     ) {
