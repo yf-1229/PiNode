03f60fd698013aeea6190e351231990d46263810
yf1229 <yf1229@proton.me>
2025-02-13 17:10:07 +0900

    PiNodeItem 表示をテキストに変更

--- app/src/main/java/com/pinode/ui/home/HomeScreen.kt
+++ app/src/main/java/com/pinode/ui/home/HomeScreen.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.pinode.PiNodeTopAppBar
 import com.pinode.data.Node
@@ -193,11 +194,10 @@ private fun PiNodeItem(
     modifier: Modifier = Modifier
 ) {
     val itemColor: Int = item.status.color
-    Box(
+    Text(
+        item.title,
+        color = colorResource(itemColor),
+        fontSize = 30.sp,
         modifier = modifier
-            .size(40.dp) // 丸のサイズ
-            .clip(CircleShape) // 丸い形状にクリップ
-            .background(colorResource(itemColor))
     )
 }
-
