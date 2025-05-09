afd7b10117a4aecfda7aadf03fb0f3818cef60e1
yf-phone <kituneter1234@gmail.com>
2025-05-09 16:11:02 +0900

    update FutureScreen.kt

--- app/src/main/java/com/pinode/ui/home/FutureScreen.kt
+++ app/src/main/java/com/pinode/ui/home/FutureScreen.kt
@@ -1,2 +1,17 @@
 package com.pinode.ui.home
 
+object AfterTomorrowDestination : NavigationDestination {
+    override val route = "aftertomorrow"
+    override val titleRes = R.string.today_title
+} 
+
+@Composable
+fun AfterTomorrowScreen(
+    navigateToNodeEntry: () -> Unit,
+    navigateToNodeEdit: (Int) -> Unit,
+    navController: NavController,
+    viewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory),
+    modifier: Modifier = Modifier,
+) {
+  
+}
