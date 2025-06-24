007491ecea15912ba04b834a1c45522e3d6457d6
yf1229 <yf1229@proton.me>
2025-06-25 07:18:19 +0900

    TODO:SplitButtonLayoutをDialogに？

--- app/src/main/java/com/pinode/ui/home/HomeScreen.kt
+++ app/src/main/java/com/pinode/ui/home/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.pinode.ui.home
 
 import androidx.activity.compose.BackHandler
 import androidx.annotation.StringRes
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
@@ -22,6 +23,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -32,18 +34,27 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Bolt
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonMenu
 import androidx.compose.material3.FloatingActionButtonMenuItem
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SplitButtonDefaults
+import androidx.compose.material3.SplitButtonLayout
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.ToggleFloatingActionButton
@@ -68,6 +79,7 @@ import androidx.compose.ui.semantics.isTraversalGroup
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.input.pointer.pointerInput
@@ -328,6 +340,7 @@ private fun PiNodeList(
 }
 
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 private fun PiNodeItem(
     item: Node,
@@ -399,6 +412,75 @@ private fun PiNodeItem(
                     )
                 }
         ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                var checked by remember { mutableStateOf(false) }
+                Box(
+                    modifier = Modifier
+                        .size(20.dp)
+                        .clip(CircleShape)
+                        .background(colorResource(item.status.color))
+                )
+                Box(modifier = Modifier.fillMaxSize().wrapContentSize()) {
+                    SplitButtonLayout(
+                        leadingButton = {
+                            SplitButtonDefaults.LeadingButton(onClick = { }) {
+                                Icon(
+                                    Icons.Filled.Edit,
+                                    modifier = Modifier.size(SplitButtonDefaults.LeadingIconSize),
+                                    contentDescription = "Localized description",
+                                )
+                                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                                Text("My Button")
+                            }
+                        },
+                        trailingButton = {
+                            SplitButtonDefaults.TrailingButton(
+                                checked = checked,
+                                onCheckedChange = { checked = it },
+                                modifier =
+                                Modifier.semantics {
+                                    stateDescription = if (checked) "Expanded" else "Collapsed"
+                                    contentDescription = "Toggle Button"
+                                },
+                            ) {
+                                val rotation: Float by
+                                animateFloatAsState(
+                                    targetValue = if (checked) 180f else 0f,
+                                    label = "Trailing Icon Rotation",
+                                )
+                                Icon(
+                                    Icons.Filled.KeyboardArrowDown,
+                                    modifier =
+                                    Modifier.size(SplitButtonDefaults.TrailingIconSize).graphicsLayer {
+                                        this.rotationZ = rotation
+                                    },
+                                    contentDescription = "Localized description",
+                                )
+                            }
+                        },
+                    )
+                    DropdownMenu(expanded = checked, onDismissRequest = { checked = false }) {
+                        DropdownMenuItem(
+                            text = { Text("Edit") },
+                            onClick = { /* Handle edit! */ },
+                            leadingIcon = { Icon(Icons.Outlined.Edit, contentDescription = null) },
+                        )
+                        DropdownMenuItem(
+                            text = { Text("Settings") },
+                            onClick = { /* Handle settings! */ },
+                            leadingIcon = { Icon(Icons.Outlined.Settings, contentDescription = null) },
+                        )
+                        HorizontalDivider()
+                        DropdownMenuItem(
+                            text = { Text("Send Feedback") },
+                            onClick = { /* Handle send feedback! */ },
+                            leadingIcon = { Icon(Icons.Outlined.Email, contentDescription = null) },
+                            trailingIcon = { Text("F11", textAlign = TextAlign.Center) },
+                        )
+                    }
+                }
+
+            }
             Text(
                 text = remainingTime.toString(),
                 color = Color.Gray,
