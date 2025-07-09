8469db64cecb114010f0feb40a388da3fc0a8b0a
yf1229 <yf1229@proton.me>
2025-07-10 07:17:49 +0900

    Add ScrapScreeen

--- app/src/main/java/com/pinode/ui/home/ScrapScreen.kt
+++ app/src/main/java/com/pinode/ui/home/ScrapScreen.kt
@@ -1,2 +1,15 @@
 package com.pinode.ui.home
 
+import androidx.compose.runtime.Composable
+import com.pinode.R
+import com.pinode.ui.navigation.NavigationDestination
+
+object ScrapDestination : NavigationDestination {
+    override val route = "scrap"
+    override val titleRes = R.string.today_title // TODO
+}
+
+@Composable
+fun ScrapScreen() {
+
+}
