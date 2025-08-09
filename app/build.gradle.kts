1965722c0bfa17f95cf9d3ce480a9c78a2ddc557
yf1229 <yf1229@proton.me>
2025-08-09 19:57:09 +0900

    Add androidx.material3.jvmstubs dependency and update version in libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.graphics.shapes)
     implementation(libs.androidx.ui.text.google.fonts)
+    implementation(libs.androidx.material3.jvmstubs)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
